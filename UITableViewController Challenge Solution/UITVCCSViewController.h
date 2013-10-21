8c8e586100d76d020da0b1ab92be16c9ea5559b4
Alex Paul <axp_alpaul@yahoo.com>
2013-10-21 06:18:15 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/UITVCCSViewController.h
@@ -0,0 +1,13 @@
+//
+//  UITVCCSViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Alex Paul on 10/21/13.
+//  Copyright (c) 2013 Alex Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITVCCSViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/UITVCCSViewController.m
@@ -0,0 +1,29 @@
+//
+//  UITVCCSViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Alex Paul on 10/21/13.
+//  Copyright (c) 2013 Alex Paul. All rights reserved.
+//
+
+#import "UITVCCSViewController.h"
+
+@interface UITVCCSViewController ()
+
+@end
+
+@implementation UITVCCSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
